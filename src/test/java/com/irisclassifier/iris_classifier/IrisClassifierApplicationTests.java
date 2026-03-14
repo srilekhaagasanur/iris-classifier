117b2e3c1aaaa68a0a3e1611dcad969f9e14f30b
Srilekha Agasanur <srilekhaagasanur@Mac.attlocal.net>
2026-03-14 13:47:58 -0500

    initial commit - spring boot iris classifier base project

---
+++ src/test/java/com/irisclassifier/iris_classifier/IrisClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisclassifier.iris_classifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
